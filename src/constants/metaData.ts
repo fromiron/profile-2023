484b92119fa0502f91c2a1e7b50675bb3aaffe27
Grayson Lee <artless1112@gmail.com>
2023-09-27 14:10:15 +0900

    基本メタデータ指定

---
+++ src/constants/metaData.ts
@@ -0,0 +1,16 @@
+const META_DATA = {
+  TITLE: "就活中！",
+  AUTHOR: "JC.Lee",
+  HEADER_TITLE: "Next.js Blog",
+  DESCRIPTION: "就活中です。ご連絡お待ちしております！",
+  LANGUAGE: "ja-JP",
+  THEME: "system",
+  SITE_URL: "https://fromiron.github.io",
+  SITE_LOGO: "/logo.png",
+  SOCIAL_BANNER: "/iam.png",
+  EMAIL: "artless1112@gmail.com",
+  GITHUB: "https://github.com/fromiron",
+  LOCALE: "ja-JP",
+};
+
+export default META_DATA;
