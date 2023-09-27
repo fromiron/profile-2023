ff8df31fc515d5e1d80e02c70e6232f78a302f0d
Grayson Lee <artless1112@gmail.com>
2023-09-27 14:11:41 +0900

    workページ基本デザイン作成

--- src/app/work.tsx
+++ src/app/work.tsx
@@ -2,37 +2,66 @@
 import ImageSlider from "@/components/ui/image-slider";
 import useViewportAction from "@/hooks/useViewportAction";
 import useNavigationStore from "@/store/navigation-store";
-import { useRef } from "react";
-import {  BackendBadge, DesignBadge, FrontendBadge } from "@/components/ui/badge"
+import { useRef, useState } from "react";
+import { Badge } from "@/components/ui/badge";
+import { allWorks } from "@/contentlayer/generated";
+import { FONT_EN } from "@/styles/fonts";
+import { Button } from "@/components/ui/button";
 
-const images = ["/work/1.png", "/work/2.png", "/nextjs.png"];
+const WORK_DISPLAY_LIMIT = 5;
 
 export default function WorkSection() {
+  const [index, setIndex] = useState(0);
   const { setActiveLink } = useNavigationStore();
   const targetRef = useRef(null);
   const onViewportEnter = () => {
     setActiveLink("/#work");
   };
   useViewportAction({ callback: onViewportEnter, ref: targetRef });
+  const limitedWorks = allWorks.slice(0, WORK_DISPLAY_LIMIT);
+  const mainImages = limitedWorks.map((work) => work.image);
+  const work = allWorks[index];
+
   return (
     <section ref={targetRef} id="work">
-      <div className="lg:flex">
-        <div className="flex-1">
-          <div className="max-w-sm mx-auto">
-            <ImageSlider images={images} />
-          </div>
-        </div>
-        <div className="flex-1 p-4">
-          <div className="">
-            美容サロン
-          </div>
-          <div className="flex gap-2">
-            <DesignBadge/>
-            <BackendBadge/>
-            <FrontendBadge/>
-          </div>
-        </div>
+      <div className="w-full flex flex-col justify-center py-10 lg:py-20">
+        <div>私はこういうことができます。</div>
+        <Button variant="outline" size={"lg"}>
+          ワークページから詳しくみる
+        </Button>
       </div>
+      <div className="mt-10 grid max-w-sm md:max-w-full mx-auto grid-cols-1 lg:grid-cols-2  justify-center">
+        <div className="w-full aspect-square max-w-sm mx-auto">
+          <ImageSlider images={mainImages} setIndex={setIndex} index={index} />
+        </div>
+        <Description
+          title={work.title}
+          tags={work.tags}
+          description={work.description}
+        />
+      </div>
+      {/* TODO　ワークページに移動するボタンデザイン */}
     </section>
+  );
+}
+
+interface DescriptionProps {
+  title: string;
+  tags?: string[];
+  description: string;
+}
+function Description({ title, tags, description }: DescriptionProps) {
+  return (
+    <div className="mt-12">
+      <div className="font-bold text-4xl mb-4 text-primary">{title}</div>
+      <div className="flex gap-2 w-full">
+        {tags?.map((tag) => (
+          <Badge variant="default" className={FONT_EN} key={tag}>
+            {tag}
+          </Badge>
+        ))}
+      </div>
+      <div className="mt-8">{description}</div>
+    </div>
   );
 }
---
+++ src/app/works/[slug]/page.tsx
@@ -0,0 +1,138 @@
+import { allWorks } from "@/contentlayer/generated";
+import Image from "next/image";
+import MdxRenderer from "@/components/mdx-renderer";
+import { notFound } from "next/navigation";
+import META_DATA from "@/constants/metaData";
+import textSplitter from "@/lib/textSlicer";
+
+export async function generateStaticParams() {
+  return allWorks.map((work) => ({ slug: work._raw.flattenedPath }));
+}
+
+export async function generateMetadata({ params }: any) {
+  const work = allWorks.find((work) => work._raw.flattenedPath === params.slug);
+  if (!work) {
+    return;
+  }
+
+  const publishedAt = new Date(work.publishedAt).toISOString();
+  const modifiedAt = new Date(work.updatedAt || work.publishedAt).toISOString();
+
+  let imageList = [META_DATA.SOCIAL_BANNER];
+  if (work.image) {
+    imageList =
+      typeof work.image === "string"
+        ? [META_DATA.SITE_URL + work.image.replace("../public", "")]
+        : work.image;
+  }
+  const ogImages = imageList.map((img) => {
+    return { url: img.includes("http") ? img : META_DATA.SITE_URL + img };
+  });
+
+  const authors = work?.author ? [work.author] : META_DATA.AUTHOR;
+
+  return {
+    title: work.title,
+    description: work.description,
+    openGraph: {
+      title: work.title,
+      description: work.description,
+      url: META_DATA.SITE_URL + work.url,
+      siteName: META_DATA.TITLE,
+      locale: META_DATA.LOCALE,
+      type: "article",
+      publishedTime: publishedAt,
+      modifiedTime: modifiedAt,
+      images: ogImages,
+      authors: authors.length > 0 ? authors : [META_DATA.AUTHOR],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: work.title,
+      description: work.description,
+      images: ogImages,
+    },
+  };
+}
+
+async function getMarkdownFromSlug(slug: string) {
+  const work = allWorks.find(
+    (work) => work._raw.flattenedPath.replace("works/", "") === slug,
+  );
+  if (!work) {
+    notFound();
+  }
+  return work;
+}
+
+interface PageProps {
+  params: { slug: string };
+}
+export default async function WorkPage({ params: { slug } }: PageProps) {
+  const work = await getMarkdownFromSlug(slug);
+
+  return (
+    <section>
+      <article>
+        <div className="group relative flex h-[50vh] w-full items-center justify-center">
+          <h1 className="absolute z-50 mx-4 flex h-auto items-center justify-center overflow-hidden rounded-lg bg-primary-foreground px-12 py-8 text-4xl font-medium transition-all">
+            {work?.title}
+            <div className="absolute left-4 top-4 -z-10 aspect-square h-4 w-4 rounded-full bg-primary" />
+          </h1>
+
+          <div className="h-full w-full overflow-hidden rounded-xl border border-transparent transition duration-500 group-hover:border-primary">
+            <Image
+              src={work?.image ?? ""}
+              alt={work?.title ?? "work main image"}
+              width={400}
+              height={400}
+              className="h-full w-full object-cover object-center transition duration-500 group-hover:blur-sm group-hover:grayscale "
+              priority
+              sizes="100vw"
+            />
+            d d
+          </div>
+        </div>
+        <div className="sxl:gap-16 mt-8  grid grid-cols-12 gap-y-8 px-5 md:px-10 lg:gap-8">
+          <div className="col-span-12  lg:col-span-4">
+            <details
+              className="border-dark dark:border-light text-dark dark:text-light sticky top-6 max-h-[80vh] overflow-hidden overflow-y-auto rounded-lg border-[1px] border-solid p-4"
+              open
+            >
+              <summary className="cursor-pointer text-lg font-semibold capitalize">
+                Table Of Content
+              </summary>
+              <ul className="font-in mt-4 text-base">
+                {work.toc.map((heading: any) => {
+                  return (
+                    <li key={`#${heading.slug}`} className="py-1">
+                      <a
+                        href={`#${heading.slug}`}
+                        data-level={heading.level}
+                        className="flex
+                                  items-center justify-start border-solid
+                                  data-[level=two]:border-t
+                                  data-[level=three]:pl-4
+                                  data-[level=two]:pl-0 data-[level=two]:pt-2 sm:data-[level=three]:pl-6
+                                  "
+                      >
+                        {heading.level === "three" ? (
+                          <span className="mr-2 flex h-1 w-1 rounded-full">
+                            &nbsp;
+                          </span>
+                        ) : null}
+
+                        <span className="hover:underline">{heading.text}</span>
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
+            </details>
+          </div>
+          <MdxRenderer mdx={work} />
+        </div>
+      </article>
+    </section>
+  );
+}
