c1e2fa2a2376f9692c18eeaa3a46abe2882d5475
Grayson Lee <artless1112@gmail.com>
2023-10-18 13:04:24 +0900

    Homeのimport 順番修正

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,8 @@
-import AboutSection from "./about";
-import ContactSection from "./contact";
-import SkillSection from "./skill";
 import TopSection from "./top";
+import AboutSection from "./about";
+import SkillSection from "./skill";
 import WorkSection from "./work";
+import ContactSection from "./contact";
 
 export default function Home() {
   return (
